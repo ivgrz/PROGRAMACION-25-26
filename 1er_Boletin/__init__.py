94161e20324d0b10624f30b158b9666a4596f059
ivgrz <igutierrezcontreras@danielcastelao.org>
2025-09-10 14:19:21 +0200

    SumaVariables_Decimales

--- 1er_Boletin/__init__.py
+++ 1er_Boletin/__init__.py
@@ -4,9 +4,18 @@ unaVariable = 9
 
 otraVariable = 10
 
+variabled = 5.3
+
+vard2 = 2.5
+
 
 #Suma de variables
 
 suma = unaVariable + otraVariable
 
 print ("La suma es: " + str(suma))
+
+
+suma2 = vard2 + variabled
+
+print ("La suma de los decimales es: " + str(suma2))
