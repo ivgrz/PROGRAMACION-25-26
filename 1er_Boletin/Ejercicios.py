e104d13258d88e5db58f9368292b357a99150891
ivnan <igutierrezcontreras@danielcastelao.org>
2025-09-12 11:50:40 +0200

    Boletin1_2

--- 1er_Boletin/Ejercicios.py
+++ 1er_Boletin/Ejercicios.py
@@ -16,3 +16,47 @@ print((9*2)/(3*25*3))
 
 print(((7*3)-(4*4)*2)/(4*2))
 
+# Ejercicio3
+
+
+m = 5
+
+n = 4
+
+p = 2
+
+r = 10
+
+s = 6
+
+t = 7
+
+q = 12
+
+i = 15
+
+c = 9
+
+resultado_a = (m + n) / n
+print("Resultado a):", resultado_a)
+
+
+resultado_b = ((m + n) / p) / ((p - r) / s)
+print("Resultado b):", resultado_b)
+
+resultado_c = (m + 4) / (p - q)
+print("Resultado c):", resultado_c)
+
+
+resultado_d = (c * r * t) / 100
+print("Resultado d):", resultado_d)
+
+resultado_e = (m + n) / (p + q / r)
+print("Resultado e):", resultado_e)
+
+resultado_f = (m / n) * (p + q)
+print("Resultado f):", resultado_f)
+
+
+resultado_g = (n * (1 + i)**t * i) / ((1 + i)**t - 1)
+print("Resultado g):", resultado_g)
