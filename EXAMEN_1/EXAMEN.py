5aabf602e9842de9ad0d34521aabfa19a6d99b57
ivgrz <igutierrezcontreras@danielcastelao.org>
2025-10-29 13:36:33 +0100

    examen

---
+++ EXAMEN_1/EXAMEN.py
@@ -0,0 +1,32 @@
+"""
+
+"""
+def temperaturas_():
+    temperatura = []
+    dias = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+
+    for i in dias:
+        temp = int(input(f"Escribe la temperatura del dia {i}: "))
+        temperatura.append(temp)
+
+        print(f"En el dia {i} la temperatura es de {temp}")
+        print(temperatura)
+
+temperaturas_()
+
+
+
+def media_temperaturas(temperatura):
+
+
+    valor_medio = sum(temperatura)/len(temperatura)
+
+    print(f"SEGUNDA FUNCION")
+    print("La lista pasada es: ", temperatura)
+    print(f"El valor medio de los grados de temperatura de esta semana es {valor_medio}")
+
+
+
+media_temperaturas(temperatura=[20,15,18,30,25])
+
+
